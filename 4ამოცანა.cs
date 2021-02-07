61aa58c7b64bc0b435b2f485491e3da292eedeec
Mmobius <70912606+Mmobius@users.noreply.github.com>
2021-02-07 09:13:46 +0400

    Add files via upload

---
+++ 4ამოცანა.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+public class Program {
+    public static void Main() {
+        bool text = IsProperly(")()()())");
+        Console.WriteLine(text);
+
+    }
+
+    static bool IsProperly(string text) {
+        char openBracket = '(';
+        char closeBracket = ')';
+
+        var stack = new Stack < char > ();
+
+        foreach(char item in text) {
+            if (item == openBracket) {
+                stack.Push(item);
+            } else if (item == closeBracket) {
+                if (stack.Count > 0 && stack.Peek() == openBracket) {
+                    stack.Pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.Count == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
