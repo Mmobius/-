219ba03753a53f13705e8f561a364d7a54600c8a
Mmobius <70912606+Mmobius@users.noreply.github.com>
2021-02-07 09:13:07 +0400

    Add files via upload

---
+++ 2ამოცანა.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+public class Program {
+    public static void Main() {
+        var x = MinSplit(156);
+
+        for (int i = 0; i < x.Count; i++) {
+            Console.WriteLine(x[i]);
+        }
+    }
+
+    static List < int > MinSplit(int amount) {
+        int[] cents = {
+            50,
+            20,
+            10,
+            5,
+            1
+        };
+        var list = new List < int > ();
+
+        for (int i = 0; i < 5; i++) {
+            int x = amount / cents[i];
+            int y = amount % cents[i];
+
+            list.Add(x);
+            if (y == 0) {
+                return list;
+            }
+
+            amount = y;
+        }
+
+        return list;
+    }
+
+}
