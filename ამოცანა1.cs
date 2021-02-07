42079e1e5473d7bde7dd3de05569eaeb6566b756
Mmobius <70912606+Mmobius@users.noreply.github.com>
2021-02-07 17:40:52 +0400

    Add files via upload

---
+++ ამოცანა1.cs
@@ -0,0 +1,40 @@
+/*
+   დაწერეთ ფუნქცია 
+   რომელსაც გადაეცემა ტექსტი  და აბრუნებს პალინდრომია თუ არა. 
+   (პალინდრომი არის ტექსტი რომელიც ერთნაირად იკითხება ორივე მხრიდან)
+*/
+
+using System;
+using System.Collections.Generic;
+
+public class Program {
+    public static void Main() 
+    {
+
+        string firsText = "Red rum, sir, is murder";
+        string secondText = "Was it a cat I saw?";
+
+        Console.WriteLine(firsText + "-{0}", IsPalindrome(firsText));
+        Console.WriteLine(secondText + "-{0}", IsPalindrome(secondText));
+    }
+
+    static bool IsPalindrome(string text) 
+    {
+        var list = new List < char > ();
+
+        foreach(var item in text) 
+        {
+            if (Char.IsLetterOrDigit(item)) 
+            {
+                list.Add(char.ToUpper(item));
+            }
+        }
+
+        text = string.Join("", list);
+        list.Reverse();
+        string reverseText = string.Join("", list);
+
+        return text == reverseText;
+    }
+
+}
